431bf4883f375caf6ded84b4f90a0e122b44f36f
Shameemah <Shameemah@shameemah.local>
2018-12-05 17:19:40 -0500

    Add js code to toggle navbar

---
+++ js/site.js
@@ -0,0 +1,6 @@
+let nav = document.getElementById('js-men'),
+    navTog = document.getElementById('js-nav-tog');
+
+navTog.addEventListener('click', function () {
+    nav.classList.toggle('show');
+});
